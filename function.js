cdcff45cb2c8ea0314961145c70a6e2936ce9244
samad <yy5841453@gmail.com>
2022-02-02 13:14:32 +0600

    calculator added

--- function.js
+++ function.js
@@ -10,9 +10,33 @@ function msh(time,current){
     return (360 * current) / time;
 }
 
-let value_arr = [];
+
+/**
+ * function for analogue clock
+ * @param {*} value 
+ */
+let resive;
+ let value_arr = [];
 let get_val = (value) =>{
     value_arr.push(value);
-    document.querySelector('.monitor-top').innerHTML = value_arr.join('')
+    document.querySelector('.monitor-top').innerHTML = value_arr.join('');
+    document.querySelector('.main-monitor').innerHTML = 0;
+};
+
+
+let all_clear = () =>{
+    value_arr = [];
+    document.querySelector('.monitor-top').innerHTML = '';
+    document.querySelector('.main-monitor').innerHTML = 0;
 }
 
+
+
+
+let calculet = () =>{
+    document.querySelector('.main-monitor').innerHTML = eval(value_arr.join(''));
+}
+let back = () =>{
+    value_arr.pop();
+    document.querySelector('.monitor-top').innerHTML = value_arr.join('');
+}
